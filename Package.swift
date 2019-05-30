9987e61730acc387a26058cced9c4a778af7a4d2
David Ben-Yaakov <david@tangogolfdigital.com>
2019-05-30 15:45:34 -0500

    Converted to dynamic library

--- Package.swift
+++ Package.swift
@@ -8,8 +8,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Igis",
-            targets: ["Igis"]),
+          name: "Igis",
+          type: .dynamic,
+          targets: ["Igis"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
