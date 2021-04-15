6eb403e79ff2ede9fea43152cce2fdbecfd0e4a9
Yong Yan <yong-yan@codermerlin.com>
2021-04-15 15:04:10 -0500

    Preamble updated

--- Sources/Igis/Layout.swift
+++ Sources/Igis/Layout.swift
@@ -1,6 +1,6 @@
 /*
 IGIS - Remote graphics for Swift on Linux
-Copyright (C) 2020 Tango Golf Digital, LLC
+Copyright (C) 2020 CoderMerlin.com
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
